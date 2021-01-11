3d094b3807c0b7ddaf53aff5b132b1a2c800d7c1
ak18nvh2 <hieuhaui12@gmail.com>
2021-01-11 13:01:05 +0700

    tao

--- app/src/main/java/com/example/pokemontest/MainActivity.kt
+++ app/src/main/java/com/example/pokemontest/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.pokemontest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        Picasso.with(this)
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png")
+            .into(img_avatar)
     }
 }
